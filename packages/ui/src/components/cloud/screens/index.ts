1b8574d9da6d97174df68e3d325101b37859066c
ananya <akblockchain15@gmail.com>
2026-05-25 04:18:17 +0530

    refactor(ui): add cloud screen components

---
+++ packages/ui/src/components/cloud/screens/index.ts
@@ -0,0 +1,7 @@
+export { DottedGlowBackground } from "./dotted-glow-background";
+export { LoginScreen } from "./login-screen";
+export type { LoginScreenProps } from "./login-screen";
+export { MyMachinesScreen } from "./my-machines-screen";
+export type { MyMachinesScreenProps } from "./my-machines-screen";
+export { RemoteMachineCreateScreen } from "./remote-machine-create-screen";
+export type { RemoteMachineCreateScreenProps } from "./remote-machine-create-screen";
